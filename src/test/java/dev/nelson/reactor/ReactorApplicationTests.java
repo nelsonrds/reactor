3aab5c805fbaf2e514e6aafb7afe0271382793f4
Nelson Rodrigues <nelsonrds95@gmail.com>
2023-02-20 18:23:27 +0000

    Last test

--- src/test/java/dev/nelson/reactor/ReactorApplicationTests.java
+++ src/test/java/dev/nelson/reactor/ReactorApplicationTests.java
@@ -124,6 +124,18 @@ class ReactorApplicationTests {
 
 	}
 
-	
+	@Test
+	public void fistWithSignalFlux() {
+		Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth").delaySubscription(Duration.ofMillis(100));
+		Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+
+		Flux<String> firstFlux = Flux.firstWithSignal(slowFlux, fastFlux);
+
+		StepVerifier.create(firstFlux)
+		.expectNext("hare")
+		.expectNext("cheetah")
+		.expectNext("squirrel")
+		.verifyComplete();
+	}
 
 }
